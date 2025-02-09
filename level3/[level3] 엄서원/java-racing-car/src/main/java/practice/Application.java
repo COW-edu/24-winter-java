2ed0c041e6dc20d28b1aa4ffccbdad47d5374686
엄서원 <usw0818@gmail.com>
2025-02-09 20:31:15 +0900

    [Level 3] 3주차 엄서원 학습 PR 제출합니다.

--- level3/[level3] 엄서원/java-racing-car/src/main/java/practice/Application.java
+++ level3/[level3] 엄서원/java-racing-car/src/main/java/practice/Application.java
@@ -1,8 +1,24 @@
 package practice;
 
+import java.util.*;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    public static void enterCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = Console.readLine();
+
+        String nameList[] = names.split(",");
+        List<Car> cars = new ArrayList<>();
+        for (String name : nameList) {
+            cars.add(new Car(name));
+        }
+        Car.tryGame(cars);
+
+    }
+
     public static void main(String[] args) {
-        // TODO 구현 진행
+        enterCarName();
     }
 }
--- level3/[level3] 엄서원/java-racing-car/src/main/java/practice/Car.java
+++ level3/[level3] 엄서원/java-racing-car/src/main/java/practice/Car.java
@@ -1,5 +1,9 @@
 package practice;
 
+import java.util.*;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,5 +12,69 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void move() {
+        position++;
+    }
+
+    public static void tryGame(List<Car> cars) {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryNumber = Integer.parseInt(Console.readLine());
+
+        runGameInformation(cars, tryNumber);
+    }
+
+    public static void runGameInformation(List<Car> cars, int tryNumber) {
+        System.out.println("실행 결과");
+        for (int i = 0; i < tryNumber; i++) {
+            for (Car car : cars) {
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+                if (randomNumber >= 4) {
+                    car.move();
+                }
+            }
+            printResult(cars);
+            System.out.println();
+        }
+        decideWinners(cars);
+    }
+
+    public static void printResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.name + " : ");
+            for (int i = 0; i < car.position; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void decideWinners(List<Car> cars) {
+        int maxPosition = 0;
+        List<Car> winner = new ArrayList<Car>();
+
+        for (int i = 0; i < cars.size(); i++) {
+            if (cars.get(i).position > maxPosition) {
+                maxPosition = cars.get(i).position;
+            }
+        }
+        for (int i = 0; i < cars.size(); i++) {
+            if (cars.get(i).position == maxPosition) {
+                winner.add(cars.get(i));
+            }
+        }
+        printWinners(winner);
+
+    }
+
+    public static void printWinners(List<Car> winner) {
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winner.size(); i++) {
+            if (i == winner.size()-1) {
+                System.out.print(winner.get(i).name);
+                return;
+            }
+            System.out.print(winner.get(i).name+", ");
+        }
+    }
+
 }
